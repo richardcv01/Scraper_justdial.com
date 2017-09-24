291daf78bb47167256cbb9ef96eb6e1d88bf64da
richardcv <richardv@ukr.net>
2017-09-24 18:37:05 +0300

    evning_24.09.17

--- Pars.py
+++ Pars.py
@@ -4,14 +4,17 @@ import Scraper
 
 
 url = "https://www.justdial.com/Ahmedabad/Karvy-Stock-Broking-Ltd-Beside-Associated-Petrol-Pump-C-G-Road/079PXX79-XX79-000819424881-F9I5_BZDET?xid=QWhtZWRhYmFkIHN0b2NrIGJyb2tpbmc="
-Web = Scraper.Web_Scraper()
-html = Web.get_html(url)
-print(html)
+#Web = Scraper.Web_Scraper()
+#html = Web.get_html_one(url)
+
 
 class Parser():
 
-    def get_culuns(self, html):
+    def get_columns(self, html):
         tree = etree.HTML(html)
+        scripts = tree.xpath('.//script[@type="application/ld+json"]')[1].xpath('.//text()')[0]
+        scripts_dic = json.loads(scripts)
+        name = scripts_dic['name']
         web_site_object = tree.xpath('.//span[@class="mreinfp comp-text"]/a')
         if len(web_site_object) == 2:
             web_site = web_site_object[1].xpath('.//@href')[0]
@@ -29,6 +32,16 @@ class Parser():
         print(web_site)
         print(adress)
         print(telephone)
+        print(name)
+
+    def get_url_topic(self, html):
+        tree = etree.HTML(html)
+        urls = tree.xpath('.//li[@class="cntanr"]/@data-href')
+        print(urls)
+        print(len(urls))
+
 
 P = Parser()
-P.get_culuns(html)
+f=open('1.txt','r', encoding="utf-8")
+html = f.read()
+P.get_url_topic(html)
--- Scraper.py
+++ Scraper.py
@@ -1,19 +1,45 @@
 
 from selenium import webdriver
 import os
-
+import time
 
 phantomjs_path = os.path.abspath("G:\phantomjs\\bin\phantomjs.exe")
 
 class Web_Scraper():
 
-    def get_html(self, BASE_URL):
+    def get_html_one(self, BASE_URL):
         driver = webdriver.PhantomJS(executable_path=phantomjs_path)
         driver.set_window_size(1400, 1000)
         driver.get(BASE_URL)
         html = driver.page_source
         return html
 
-url = "https://www.justdial.com/Ahmedabad/Stock-Broking-Maninagar/079PXX79-XX79-170321181502-P4W1_BZDET?xid=QWhtZWRhYmFkIHN0b2NrIGJyb2tpbmc="
+    def get_html_topic(self, BASE_URL):
+        driver = webdriver.PhantomJS(executable_path=phantomjs_path)
+        driver.set_window_size(1400, 1000)
+        driver.get(BASE_URL)
+        SCROLL_PAUSE_TIME = 3
 
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
 
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+        html = driver.page_source
+        return html
+
+url = "https://www.justdial.com/Ahmedabad/stock-broking"
+
+Web = Web_Scraper()
+html = Web.get_html_topic(url)
+
+f=open('1.txt','w', encoding="utf-8")
+f.write(html)
